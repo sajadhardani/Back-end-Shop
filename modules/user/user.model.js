572e6bb0df7e0846c16828f9ca977da926aa6739
shcv <sajad.hardani188@gmail.com>
2026-07-07 23:31:12 +0330

    Add: User Model

--- modules/user/user.model.js
+++ modules/user/user.model.js
@@ -1,10 +1,41 @@
 const { default: mongoose } = require("mongoose");
 
-const userSchema = new mongoose.UserSchema({
-    phone:{
-        type: String,
-        required: true,
-    }
+const addressSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  postalCode: { type: String, required: true },
+  location: {
+    lat: {
+      type: Number,
+      required: truex,
+    },
+    lng: {
+      type: Number,
+      required: true,
+    },
+    cityId: {
+      type: Number,
+      required: true,
+    },
+  },
 });
 
-module.exports = mongoose.model("Ban", banSchema);
+const userSchema = new mongoose.Schema(
+  {
+    phone: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    username: {type: String, required: true},
+    
+    roles: {
+      type: [String],
+      enum: ["ADMIN", "USER", "SELLER"],
+      default: ["USER"],
+    },
+    addresses: [addressSchema],
+  },
+  { timestamps: true },
+);
+
+module.exports = mongoose.model("User", userSchema);
